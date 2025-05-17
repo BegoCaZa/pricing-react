85a8ba8544b56b07f49a200a1443d861e691d257
BegoCaZa <begoxcz@hotmail.com>
2025-05-17 08:58:10 +0200

    cómo co;o voy a hacer la sobreposicion del card en el desktop y los fondooooos
    odio position

--- src/components/cardsContainer/cardsContainer.styles.js
+++ src/components/cardsContainer/cardsContainer.styles.js
@@ -6,8 +6,8 @@ const StyledCardsContainer = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  margin-top: 20px;
-  gap: 32px;
+  margin-top: 1.25rem;
+  gap: 2rem;
 
   //Web
   @media (min-width: 1080px) {
@@ -20,8 +20,8 @@ const StyledCardsContainer = styled.div`
 const StyledCard = styled.div`
   background: ${({ plan }) =>
     plan === 'Professional' ? COLORS.lilac : COLORS.white};
-  border-radius: 16px;
-  box-shadow: 0 2px 8px rgba(0,0,0,0.1);
+  border-radius: 1rem;
+  box-shadow: 0 0.125rem 0.5rem rgba(0,0,0,0.1);
   padding: 2rem;
   display: flex;
   flex-direction: column;
@@ -29,11 +29,11 @@ const StyledCard = styled.div`
   margin-inline: auto;
   align-items: center;
   justify-content: center;
-    gap: 32px;
+    gap: 2rem;
   }
 `;
 const StyledPlanTitle = styled.p`
-  font-size: 18px;
+  font-size: 1.125rem;
   font-weight: 700;
   margin: 0;
   color: ${({ plan }) =>
@@ -44,16 +44,16 @@ const StyledInfoContainer = styled.div`
   flex-direction: column;
   gap: 0;
   width: 100%;
-  gap: 8px;
+  gap: 0.5rem;
   margin: 0;
   justify-content: center;
   align-items: center;
 `;
 const StyledPrice = styled.p`
-  font-size: ${({ active }) => (active ? '40px' : '68px')};
+  font-size: ${({ active }) => (active ? '2.5rem' : '4.25rem')};
   font-weight: 700;
   margin: 0;
-  padding: 24px;
+  padding: 1.5rem;
   color: ${({ plan }) =>
     plan === 'Professional' ? COLORS.white : COLORS.darkGrey};
 `;
@@ -63,17 +63,17 @@ const StyledStorageUsersContainer = styled.div`
   flex-direction: column;
   gap: 0;
   width: 100%;
-  border-top: 1px solid
+  border-top: 0.0625rem solid
     ${({ plan }) =>
       plan === 'Professional' ? COLORS.lightWhite : COLORS.lightGrey};
 `;
 const StyledCardPlanInfoText = styled.p`
-  font-size: 15px;
+  font-size: 0.9375rem;
   font-weight: 600;
-  border-bottom: 1px solid
+  border-bottom: 0.0625rem solid
     ${({ plan }) =>
       plan === 'Professional' ? COLORS.lightWhite : COLORS.lightGrey};
-  padding: 14px;
+  padding: 0.875rem;
   margin: 0;
   width: 100%;
   text-align: center;
@@ -87,14 +87,14 @@ const StyledButton = styled.button`
   color: ${({ plan }) =>
     plan === 'Professional' ? COLORS.grey : COLORS.white};
   border: none;
-  border-radius: 8px;
+  border-radius: 0.625rem;
   padding: 0.5rem 1rem;
   cursor: pointer;
-  font-size: 12px;
+  font-size: 0.75rem;
   font-weight: 700;
   margin: 0;
   width: 100%;
-  padding: 16px;
+  padding: 1rem;
 
   &:hover {
     background: ${({ plan }) =>
