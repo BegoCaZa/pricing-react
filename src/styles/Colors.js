59c320a0cee45d18bbf5249047764c4a95e521d0
BegoCaZa <begoxcz@hotmail.com>
2025-05-17 08:52:14 +0200

    d

--- src/styles/Colors.js
+++ src/styles/Colors.js
@@ -2,7 +2,7 @@ export const COLORS = {
   white: '#ffffff',
   lightWhite: 'rgba(255, 255, 255, 0.38)',
   lilac: 'linear-gradient(135deg, #a2a7f0 0%, #696edd 100%)',
-  lilacSolid: '6D72DE',
+  lilacSolid: '#6D72DE',
   darkGrey: '#4A4D60',
   grey: '#6E728e',
   lightGrey: 'rgba(110,114,142,0.5)'
